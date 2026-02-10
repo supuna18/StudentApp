215fef96ff9fb33337428e330c0938adce94df8b
Supun Anjana <supuna793@gmail.com>
2026-02-11 00:19:16 +0530

    Add features.js to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Features from "./components/Features";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       {/* ප්‍රධාන පෙනුම (Hero Section) */}
       <main>
         <Hero />
+      <Features /> {/* අලුතින් හැදූ කොටස */}
       </main>
 
       {/* ඊළඟට අපි මෙතනට Modules Section එක එකතු කරමු */}
